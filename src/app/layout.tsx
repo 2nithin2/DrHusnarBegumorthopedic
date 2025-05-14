80bc963fb6afc5f8294382a7fd5547fd7235e1c3
K.Nithin Raj <official.nithinraj@gmail.com>
2025-05-14 13:19:44 +0000

    be as designer add some light gradient colors in bg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body
         className={cn(
-          'min-h-screen bg-background font-sans antialiased',
+          'min-h-screen font-sans antialiased', // Removed bg-background
           openSans.variable
         )}
       >
